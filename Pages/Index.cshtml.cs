d6fb4d60da9f0de03f830d93e7b68cc49eabfe76
dlucas32 <32davidl@gmail.com>
2022-05-20 00:02:20 -0700

    figuring out context DI + html

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,21 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using League.Data;
+using League.Models;
 
 namespace League.Pages
 {
   public class IndexModel : PageModel
   {
     private readonly ILogger<IndexModel> _logger;
+    private readonly LeagueContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, LeagueContext context)
     {
       _logger = logger;
+      _context = context;
     }
-
-    public void OnGet()
+    public League.Models.League League { get; set; }
+    public async Task OnGetAsync()
     {
-
+        League = await _context.Leagues.FirstOrDefaultAsync();
     }
   }
 }
